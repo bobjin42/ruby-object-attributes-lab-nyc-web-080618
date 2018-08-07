84985a8d03f7d37fe34433c451f62e0910c0e8aa
Yicheng Jin <bobjin42@gmail.com>
2018-08-07 15:43:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,4 +10,16 @@ class Dog
   def name=(new_name)
     @name = new_name
   end
+  
+  def breed(name)
+    @breed = name
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(new_name)
+    @breed = new_name
+  end
 end
